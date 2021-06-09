97f734b4ebb156b7896c3175abeb766e9c64e236
Joe Schoonover <joe@fluidnumerics.com>
2021-06-09 22:09:41 +0000

    Add terraform deployment for gromacs

--- gromacs/install.sh
+++ gromacs/install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #
 #
-# Maintainers : @your-github-handle-here
+# Maintainers : @schoonovernumerics
 #
 # //////////////////////////////////////////////////////////////// #
 
 # Set any package variables here
-INSTALL_ROOT="/opt"
-SPACK_VERSION="releases/latest"
+SPACK_VERSION="develop"
 GCC_VERSION="9.2.0"
 OPENMPI_VERSION="4.0.5"
+GMX_VERSION="2021.1"
+ARCH="x86_64"
 ###
 
 
@@ -21,10 +22,16 @@ spack_setup() {
   echo "export SPACK_ROOT=${INSTALL_ROOT}/spack" > /etc/profile.d/spack.sh
   echo ". \${SPACK_ROOT}/share/spack/setup-env.sh" >> /etc/profile.d/spack.sh
   source ${INSTALL_ROOT}/spack/share/spack/setup-env.sh
+{
+  echo "config:"
+  echo "  install_tree: ${INSTALL_ROOT}/software"
+} >> ${INSTALL_ROOT}/spack/etc/spack/config.yaml
+
   spack compiler find --scope site
   
   # Install lmod for module managament
-  spack install lmod
+  spack install lmod arch=$ARCH
+
   source $(spack location -i lmod)/lmod/lmod/init/bash
   source ${INSTALL_ROOT}/spack/share/spack/setup-env.sh
   echo "source $(spack location -i lmod)/lmod/lmod/init/bash" >> /etc/profile.d/spack.sh
@@ -72,15 +79,56 @@ spack_setup
 export PATH=$PATH:/usr/local/cuda
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
 spack external find --scope site cuda
+# Spack is often unable to find slurm
+{
+  echo "  slurm:"
+  echo "    externals:"
+  echo "    - spec: slurm@20-11"
+  echo "      prefix: ${SLURM_ROOT}"
+} >> ${INSTALL_ROOT}/spack/etc/spack/packages.yaml
 
 # Install GCC compiler (newer version than system compiler)
 spack install gcc@${GCC_VERSION}
 spack load gcc@${GCC_VERSION}
-spack compiler find
+spack compiler find --scope site
 
-spack install gromacs+cuda~mpi ^fftw~mpi
+spack install --source --fail-fast gromacs@${GMX_VERSION}+cuda+mpi ^fftw+mpi ^openmpi@${OPENMPI_VERSION}+cuda+cxx+cxx_exceptions+legacylaunchers+memchecker+pmi+static+vt+wrapper-rpath fabrics=auto schedulers=slurm target=${ARCH}
+
+spack gc -y
 
 lmod_setup
+
+# Update MOTD
+cat > /etc/motd << EOL
+=======================================================================  
+  Gromacs-GCP VM Image
+  Copyright 2021 Fluid Numerics LLC
+
+=======================================================================  
+
+  Open source implementations of this solution can be found at
+
+    https://github.com/FluidNumerics/hpc-apps-gcp/gromacs
+
+  This solution contains free and open-source software 
+  All applications installed can be listed using 
+
+  spack find
+
+  You can obtain the source code and licenses for any 
+  installed application using the following command :
+
+  ls \$(spack location -i pkg)/share/pkg/src
+
+  replacing "pkg" with the name of the package.
+
+=======================================================================  
+
+  To get started, check out the included docs
+
+    cat ${INSTALL_ROOT}/share/doc
+
+EOL
 
 # Install benchmarks
 mkdir -p /apps/share/gromacs
