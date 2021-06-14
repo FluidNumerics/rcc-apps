0def70733540255859ac8436c9731c313435a4ef
Joe Schoonover <joe@fluidnumerics.com>
2021-06-14 15:23:06 +0000

    Add Singularity+OpenMPI package

---
+++ singularity/install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+#
+# Maintainers : @schoonovernumerics
+#
+# //////////////////////////////////////////////////////////////// #
+
+# Set any package variables here
+SPACK_VERSION="develop"
+###
+
+source ${INSTALL_ROOT}/spack/share/spack/setup-env.sh
+
+spack env activate ${INSTALL_ROOT}/spack-env/
+spack install --fail-fast --source
+spack gc -y
+spack env deactivate
+spack env activate --sh -d . >> /etc/profile.d/z10_spack_environment.sh 
+
+
+# Update MOTD
+cat > /etc/motd << EOL
+=======================================================================  
+  Singularity-GCP VM Image
+  Copyright 2021 Fluid Numerics LLC
+
+=======================================================================  
+
+  Open source implementations of this solution can be found at
+
+    https://github.com/FluidNumerics/hpc-apps-gcp/spack
+
+  This solution contains free and open-source software 
+  All applications installed can be listed using 
+
+  spack find
+
+  You can obtain the source code and licenses for any 
+  installed application using the following command :
+
+  ls \$(spack location -i pkg)/share/pkg/src
+
+  replacing "pkg" with the name of the package.
+
+=======================================================================  
+
+  To get started, check out the included docs
+
+    cat ${INSTALL_ROOT}/share/doc
+
+EOL
