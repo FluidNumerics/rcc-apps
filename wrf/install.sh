32165d0a0bdef447c3376040ead8b30e1ea5010e
Joe Schoonover <joe@fluidnumerics.com>
2021-07-07 17:55:58 +0000

    Add valgrind-devel package to system install
    * Valgrind headers are needed to install openmpi

--- wrf/install.sh
+++ wrf/install.sh
@@ -6,7 +6,7 @@
 # //////////////////////////////////////////////////////////////// #
 
 
-yum install -y valgrind
+yum install -y valgrind valgrind-devel
 
 sed -i 's/@INSTALL_ROOT@/${INSTALL_ROOT}/g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
 sed -i 's/@COMPILER@/${COMPILER}/g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
