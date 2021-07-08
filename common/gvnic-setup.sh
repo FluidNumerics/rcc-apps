1e1581953c61762daef3a66cc99a92163b0e4067
Joe Schoonover <joe@fluidnumerics.com>
2021-07-08 21:35:33 +0000

    Add script for installing gvnic driver from source

---
+++ common/gvnic-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://github.com/GoogleCloudPlatform/compute-virtual-ethernet-linux/releases/download/v1.2.2/gve-1.2.2.tar.gz --directory-prefix=/tmp
+cd /tmp/ && tar -xvzf gve-1.2.2.tar.gz
+cd /tmp/gve-1.1.2
+make -C /lib/modules/`uname -r`/build M=$(pwd)/build modules modules_install
+depmod
+modprobe gve
