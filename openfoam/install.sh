84be26e44b78e8f8ab1fc865dc8a3920543f45cc
Joe Schoonover <joe@fluidnumerics.com>
2021-07-15 20:03:37 +0000

    Consolidate spack package installation into common
    * This simplifies application installation and reduces duplicate code
    for each package. This does, however, require that new packages are
    brought in through spack environments; all packages require a spack.yaml
    under an `env/` subdirectory.

--- openfoam/install.sh
+++ openfoam/install.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 #
 
-yum install -y valgrind valgrind-devel
-
-sed -i 's#@INSTALL_ROOT@#'"${INSTALL_ROOT}"'#g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
-
-if [[ "$COMPILER" == *"intel"* ]]; then
-  sed -i 's/@COMPILER@/intel/g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
-else
-  sed -i 's/@COMPILER@/'"${COMPILER}"'/g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
-fi
-
-sed -i 's/@ARCH@/'"${ARCH}"'/g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
-
-source ${INSTALL_ROOT}/spack/share/spack/setup-env.sh
-
-if [[ "$IMAGE_NAME" != *"fluid-hpc"* ]]; then
-   spack install ${COMPILER}
-   spack load ${COMPILER}
-   spack compiler find --scope site
-fi
-
-spack env activate ${INSTALL_ROOT}/spack-pkg-env/
-spack install --fail-fast --source
-spack gc -y
-spack env deactivate
-spack env activate --sh -d ${INSTALL_ROOT}/spack-pkg-env/ >> /etc/profile.d/z10_spack_environment.sh 
-
 # Update MOTD
 cat > /etc/motd << EOL
 =======================================================================  
--- singularity/install.sh
+++ singularity/install.sh
@@ -6,28 +6,6 @@
 # //////////////////////////////////////////////////////////////// #
 
 
-sed -i 's#@INSTALL_ROOT@#'"${INSTALL_ROOT}"'#g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
-if [[ "$COMPILER" == *"intel"* ]]; then
-  sed -i 's/@COMPILER@/intel/g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
-else
-  sed -i 's/@COMPILER@/'"${COMPILER}"'/g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
-fi
-
-source ${INSTALL_ROOT}/spack/share/spack/setup-env.sh
-
-if [[ "$IMAGE_NAME" != *"fluid-hpc"* ]]; then
-   spack install ${COMPILER}
-   spack load ${COMPILER}
-   spack compiler find --scope site
-fi
-
-spack env activate ${INSTALL_ROOT}/spack-pkg-env/
-spack install --fail-fast --source
-spack gc -y
-spack env deactivate
-spack env activate --sh -d ${INSTALL_ROOT}/spack-pkg-env/ >> /etc/profile.d/z10_spack_environment.sh 
-
-
 # Provide conveniencce directory for bind mount
 mkdir /workspace
 chmod ugo=rwx -R /workspace
