42e031f0dec1efb3c23ab0d2e7fc51b2de962ac2
Joe Schoonover <joe@fluidnumerics.com>
2021-07-21 22:15:52 +0000

    Add FEOTS (https://github.com/fluidnumerics/FEOTS)

---
+++ feots/README.md
@@ -0,0 +1,10 @@
+# FEOTS
+Copyright 2021 Fluid Numerics LLC
+
+Maintainers: @schoonovernumerics
+
+## Getting Started
+
+
+## How to use this image
+This image installs FEOTS via spack into the `/opt` directory by default. A view of packages is configured by default under `/opt/view` and the necessary `PATH` and `LD_LIBRARY_PATH` are configured for you automatically so that necessary tools (e.g. `mpirun` and `feots`) are avaialable without additional configuration.
---
+++ feots/install.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#
+#
+# Maintainers : @schoonovernumerics
+#
+# //////////////////////////////////////////////////////////////// #
+
+
+source /etc/profile.d/z10_spack_environment.sh 
+spack load ${COMPILER} openmpi@4.0.2 % ${COMPILER}
+
+ln -s $(spack location -i netcdf-fortran)/lib/* $(spack location -i netcdf-c)/lib/
+ln -s $(spack location -i netcdf-fortran)/include/* $(spack location -i netcdf-c)/include/
+
+# Install FEOTS #
+git clone https://github.com/FluidNumerics/FEOTS.git /tmp/FEOTS
+cd /tmp/FEOTS
+autoreconf --install
+FCFLAGS="-Ofast -ffree-line-length-none" ./configure --enable-mpi --prefix=${INSTALL_ROOT}/feots
+make
+make install
+# Add the examples to the install path
+cp -r /tmp/FEOTS/examples ${INSTALL_ROOT}/feots
+
+echo "#!/bin/bash" > /etc/profile.d/z11_feots.sh
+echo "export PATH=\${PATH}:${INSTALL_ROOT}/feots/bin" >> /etc/profile.d/z11_feots.sh
+echo "export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:${INSTALL_ROOT}/feots/lib" >> /etc/profile.d/z11_feots.sh
+
+# Add Argentine Basin input decks
+mkdir -p ${INSTALL_ROOT}/feots-db
+mkdir -p ${INSTALL_ROOT}/feots-db/argentine_basin
+gsutil -m cp -r gs://feots-db/E3SMV0-HILAT-5DAVG ${INSTALL_ROOT}/feots-db
+gsutil -m cp -r gs://feots-db/argentine_basin_regional_operators/* ${INSTALL_ROOT}/feots-db/argentine_basin/
+
+
+# Update MOTD
+cat > /etc/motd << EOL
+=======================================================================  
+  FEOTS-GCP VM Image
+  Copyright 2021 Fluid Numerics LLC
+
+=======================================================================  
+
+  Open source implementations of this solution can be found at
+
+    https://github.com/FluidNumerics/hpc-apps-gcp
+
+  This solution contains free and open-source software 
+  All applications installed can be listed using 
+
+  spack find
+
+  You can obtain the source code and licenses for any 
+  installed application using the following command :
+
+  ls \$(spack location -i pkg)/share/pkg/src
+
+  replacing "pkg" with the name of the package.
+
+=======================================================================  
+
+  To get started, check out the included docs
+
+    cat ${INSTALL_ROOT}/share/doc
+
+EOL
