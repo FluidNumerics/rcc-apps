6384a9de06a39f6eb71d44d988d71b876d0f8ff3
Joe Schoonover <joe@fluidnumerics.com>
2021-07-21 18:20:33 +0000

    Consolidate common/spack scripts

--- common/spack-install.sh
+++ common/spack-install.sh
@@ -10,14 +10,14 @@ SPACK_VERSION="develop"
 BUILDCACHE="/tmp/gcp-spack-cache/${IMAGE_NAME}"
 ###
 
+yum update -y
+yum install -y valgrind valgrind-devel
 
-######################################################################################################################
-if [[ "$IMAGE_NAME" != *"fluid-hpc"* ]]; then
+if [[ "$IMAGE_NAME" != "fluid-hpc"* ]]; then
    yum install -y gcc gcc-c++ gcc-gfortran
    pip3 install google-cloud-storage
-   ## Install spack
-   # TO DO : Once  merged into main branch, bring back spack/spack repo
-   #git clone https://github.com/spack/spack.git --branch ${SPACK_VERSION} ${INSTALL_ROOT}/spack
+
+   ## Install spack (from FluidNumerics fork)
    git clone https://github.com/FluidNumerics/spack.git ${INSTALL_ROOT}/spack
    
    echo "#!/bin/bash" > /etc/profile.d/z10_spack_environment.sh
@@ -36,5 +36,29 @@ fi
 
 ## For ensuring that CUDA paths are in default path ##
 if [[ -f "/etc/profile.d/cuda.sh" ]]; then
-	mv /etc/profile.d/cuda.sh /etc/profile.d/z12_cuda.sh
+	mv /etc/profile.d/cuda.sh /etc/profile.d/z11_cuda.sh
 fi
+
+sed -i 's#@INSTALL_ROOT@#'"${INSTALL_ROOT}"'#g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
+if [[ "$COMPILER" == *"intel"* ]]; then
+  sed -i 's/@COMPILER@/intel/g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
+else
+  sed -i 's/@COMPILER@/'"${COMPILER}"'/g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
+fi
+
+sed -i 's/@ARCH@/'"${ARCH}"'/g' ${INSTALL_ROOT}/spack-pkg-env/spack.yaml
+
+source ${INSTALL_ROOT}/spack/share/spack/setup-env.sh
+
+# Fluid Numerics Images (start with "fluid")
+if [[ "$IMAGE_NAME" != "fluid"* ]]; then
+   spack install ${COMPILER}
+   spack load ${COMPILER}
+   spack compiler find --scope site
+fi
+
+spack env activate ${INSTALL_ROOT}/spack-pkg-env/
+spack install --fail-fast --source
+spack gc -y
+spack env deactivate
+spack env activate --sh -d ${INSTALL_ROOT}/spack-pkg-env/ >> /etc/profile.d/z10_spack_environment.sh 
