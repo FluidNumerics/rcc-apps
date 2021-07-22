5f3b5622a0d2864bda0d3b659dab9eec158c51ee
Joe Schoonover <joe@fluidnumerics.com>
2021-07-22 18:42:52 +0000

    Add demo script for FEOTS and update install to enable profiling with gprof

--- feots/install.sh
+++ feots/install.sh
@@ -16,7 +16,7 @@ ln -s $(spack location -i netcdf-fortran)/include/* $(spack location -i netcdf-c
 git clone https://github.com/FluidNumerics/FEOTS.git /tmp/FEOTS
 cd /tmp/FEOTS
 autoreconf --install
-FCFLAGS="-Ofast -ffree-line-length-none" ./configure --enable-mpi --prefix=${INSTALL_ROOT}/feots
+FCFLAGS="-Ofast -g -pg -ffree-line-length-none" ./configure --enable-mpi --prefix=${INSTALL_ROOT}/feots
 make
 make install
 # Add the examples to the install path
@@ -25,12 +25,28 @@ cp -r /tmp/FEOTS/examples ${INSTALL_ROOT}/feots
 echo "#!/bin/bash" > /etc/profile.d/z11_feots.sh
 echo "export PATH=\${PATH}:${INSTALL_ROOT}/feots/bin" >> /etc/profile.d/z11_feots.sh
 echo "export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:${INSTALL_ROOT}/feots/lib" >> /etc/profile.d/z11_feots.sh
+echo "export FEOTS_LIB=\"-L${INSTALL_ROOT}/feots/lib -lfeots\"" >> /etc/profile.d/z11_feots.sh
+echo "export FEOTS_INCLUDE=\"-I${INSTALL_ROOT}/feots/include" >> /etc/profile.d/z11_feots.sh
 
 # Add Argentine Basin input decks
 mkdir -p ${INSTALL_ROOT}/feots-db
-mkdir -p ${INSTALL_ROOT}/feots-db/argentine_basin
+mkdir -p ${INSTALL_ROOT}/feots-db/argentine-basin
 gsutil -m cp -r gs://feots-db/E3SMV0-HILAT-5DAVG ${INSTALL_ROOT}/feots-db
-gsutil -m cp -r gs://feots-db/argentine_basin_regional_operators/* ${INSTALL_ROOT}/feots-db/argentine_basin/
+gsutil -m cp -r gs://feots-db/argentine_basin_regional_operators/* ${INSTALL_ROOT}/feots-db/argentine-basin/
+
+
+mkdir -p ${INSTALL_ROOT}/share/argentine-basin
+source /etc/profile.d/z11_feots.sh
+cp ${INSTALL_ROOT}/feots/examples/zapiola/*.f90 \
+   ${INSTALL_ROOT}/feots/examples/zapiola/Makefile \
+   ${INSTALL_ROOT}/feots/examples/zapiola/runtime.params \
+   ${INSTALL_ROOT}/share/argentine-basin/
+
+mv /tmp/share/demo.sh ${INSTALL_ROOT}/share/argentine-basin/
+
+cd ${INSTALL_ROOT}/share/argentine-basin
+make
+
 
 
 # Update MOTD
---
+++ feots/share/demo.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+export FEOTS_DBROOT=/opt/feots-db/E3SMV0-HILAT-5DAVG
+export REGIONAL_DB=/opt/feots-db/argentine-basin
+export DEMO_DIR=/opt/feots/share/argentine-basin
+export OUTDIR=~/feots
+
+mkdir -p $OUTDIR
+cp ${DEMO_DIR}/genmask ${OUTDIR}/genmask
+cp ${DEMO_DIR}/init ${OUTDIR}/init
+cp ${DEMO_DIR}/runtime.params ${OUTDIR}/runtime.params
+
+
+cd ${OUTDIR}
+
+echo ""
+echo "================================================"
+echo "Creating mask for FEOTS simulation database"
+echo ""
+./genmask --dbroot ${FEOTS_DBROOT}  \
+          --regional-db ${REGIONAL_DB} \
+          --out ${OUTDIR} \
+          --param-file ./runtime.params
+
+echo ""
+echo "================================================"
+echo "Creating regional maps to map with global mesh"
+echo ""
+mpirun -np 1 feots genmaps --out ${OUTDIR} \
+                           --regional-db ${REGIONAL_DB} \
+                           --dbroot ${FEOTS_DBROOT} \
+                           --param-file ./runtime.params
+
+
+echo ""
+echo "================================================"
+echo "Creating initial conditions"
+echo ""
+mpirun -np 6 ./init --out ${OUTDIR} \
+                    --regional-db ${REGIONAL_DB} \
+                    --dbroot ${FEOTS_DBROOT} \
+                    --param-file ./runtime.params
+
+echo ""
+echo "================================================"
+echo "Creating initial conditions"
+echo ""
+mpirun -np 6 feots integrate ${FEOTS_FLAGS} \
+                             --regional-db ${REGIONAL_DB} \
+                             --out ${OUTDIR} \
+                             --param-file ./runtime.params
