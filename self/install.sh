9b2e18801289de7defec5b322edac29d07cdfe26
Joe Schoonover <joe@fluidnumerics.com>
2021-08-23 22:49:06 +0000

    Add cmake to environment

--- self/install.sh
+++ self/install.sh
@@ -85,19 +85,3 @@ cat > /etc/motd << EOL
     cat ${INSTALL_ROOT}/share/doc
 
 EOL
-
-# Install benchmarks
-mkdir -p ${INSTALL_ROOT}/share/gromacs
-wget https://www.mpibpc.mpg.de/15101317/benchMEM.zip -P /tmp
-wget https://www.mpibpc.mpg.de/15615646/benchPEP.zip -P /tmp
-wget https://www.mpibpc.mpg.de/17600708/benchPEP-h.zip -P /tmp
-wget https://www.mpibpc.mpg.de/15101328/benchRIB.zip -P /tmp
-
-unzip /tmp/benchMEM.zip -d ${INSTALL_ROOT}/share/gromacs
-unzip /tmp/benchPEP.zip -d ${INSTALL_ROOT}/share/gromacs
-unzip /tmp/benchPEP-h.zip -d ${INSTALL_ROOT}/share/gromacs
-unzip /tmp/benchRIB.zip -d ${INSTALL_ROOT}/share/gromacs
-
-# Ensure that input deck permissions are readable by all
-chmod 755 ${INSTALL_ROOT}/share/gromacs
-chmod 644 ${INSTALL_ROOT}/share/gromacs/*
