c78014f8db5be0162ae1a41c1ce34d54262a98f6
Joe Schoonover <joe@fluidnumerics.com>
2021-09-28 15:00:28 +0000

    Add Intel OneAPI compilers to compiler installs

--- rcc-centos/install.sh
+++ rcc-centos/install.sh
@@ -52,26 +52,28 @@ source ${INSTALL_ROOT}/spack/share/spack/setup-env.sh
 # Install "after-market" compilers
 COMPILERS=("gcc@11.2.0"
            "gcc@10.3.0"
-	   "gcc@9.4.0")
+	   "gcc@9.4.0"
+	   "intel-oneapi-compilers")
 
 for COMPILER in "${COMPILERS[@]}"; do
   spack_install "${COMPILER} % gcc@4.8.5 target=${ARCH}"
   spack load ${COMPILER} && spack compiler find --scope site && spack unload ${COMPILER}
 done
 
-spack compiler find --scope site
 # Adjust the paths to AMD Clang/Flang compilers
-sed -i "s#cc: /usr/bin/clang-ocl#cc: /opt/rocm/bin/amdclang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
+sed -i "s#.*clang-ocl.*#cc: /opt/rocm/bin/amdclang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
 sed -i "s#cxx: null#cxx: /opt/rocm/bin/amdclang++#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
 sed -i "s#f77: null#f77: /opt/rocm/bin/amdflang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
 sed -i "s#fc: null#fc: /opt/rocm/bin/amdflang#" ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
+
 cat ${INSTALL_ROOT}/spack/etc/spack/compilers.yaml
 
 # Install OpenMPI with desired compilers
 COMPILERS=("gcc@11.2.0"
            "gcc@10.3.0"
            "gcc@9.4.0"
-	   "clang@13.0.0")
+	   "clang@13.0.0"
+	   "intel")
 for COMPILER in "${COMPILERS[@]}"; do
   if [[ "$COMPILER" == *"intel"* ]];then
     spack_install "openmpi@4.0.5 % intel target=${ARCH}"
