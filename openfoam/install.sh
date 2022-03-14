a831b1c9f142a68a1426d046cc1602fbcb802697
Joe Schoonover <joe@fluidnumerics.com>
2022-03-14 15:37:01 -0600

    Switch to OSMesa paraview install

--- openfoam/install.sh
+++ openfoam/install.sh
@@ -2,7 +2,9 @@
 #
 
 # Install paraview
-wget --output-document /tmp/paraview.tar.gz "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.10&type=binary&os=Linux&downloadFile=ParaView-5.10.0-egl-MPI-Linux-Python3.9-x86_64.tar.gz"
+#wget --output-document /tmp/paraview.tar.gz "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.10&type=binary&os=Linux&downloadFile=ParaView-5.10.0-egl-MPI-Linux-Python3.9-x86_64.tar.gz"
+
+wget --output-document /tmp/paraview.tar.gz "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.10&type=binary&os=Linux&downloadFile=ParaView-5.10.0-osmesa-MPI-Linux-Python3.9-x86_64.tar.gz"
 
 mkdir /opt/paraview
 tar -xvzf /tmp/paraview.tar.gz --directory /opt/paraview --strip-components 1
