66fa52973f4249ad28af52c557691107df990144
Joe Schoonover <joe@fluidnumerics.com>
2022-03-22 17:41:46 -0600

    Update singularity and openmpi; enable profile sourcing for noninteractive shell

--- fluid-run/install.sh
+++ fluid-run/install.sh
@@ -7,4 +7,7 @@
 mkdir /apps/workspace
 chmod ugo=rwx -R /apps/workspace
 
+# Modify default bashrc to source profile, even for non-interactive shell
+echo -e "source /etc/profile \n$(cat /etc/bash.bashrc)" > /etc/bash.bashrc
+
 cat /dev/null > /var/log/messages
