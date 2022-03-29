7a238ee960fb0957914d61aa629a433442160302
Ross Thomson <jrossthomson@google.com>
2022-03-29 16:55:06 +0000

    Fix some packer and slurm build issues.

--- gromacs/install.sh
+++ gromacs/install.sh
@@ -39,16 +39,16 @@ EOL
 
 # Install benchmarks
 mkdir -p ${INSTALL_ROOT}/share/gromacs
-wget https://www.mpibpc.mpg.de/15101317/benchMEM.zip -P /tmp
-wget https://www.mpibpc.mpg.de/15615646/benchPEP.zip -P /tmp
-wget https://www.mpibpc.mpg.de/17600708/benchPEP-h.zip -P /tmp
-wget https://www.mpibpc.mpg.de/15101328/benchRIB.zip -P /tmp
+wget https://www.mpinat.mpg.de/benchMEM -O /tmp/benchMEM.zip
+wget https://www.mpinat.mpg.de/benchRIB -O /tmp/benchRIB.zip
+wget https://www.mpinat.mpg.de/benchPEP -O /tmp/benchPEP.zip
 
 unzip /tmp/benchMEM.zip -d ${INSTALL_ROOT}/share/gromacs
 unzip /tmp/benchPEP.zip -d ${INSTALL_ROOT}/share/gromacs
-unzip /tmp/benchPEP-h.zip -d ${INSTALL_ROOT}/share/gromacs
 unzip /tmp/benchRIB.zip -d ${INSTALL_ROOT}/share/gromacs
 
 # Ensure that input deck permissions are readable by all
-chmod 755 ${INSTALL_ROOT}/share/gromacs
-chmod 644 ${INSTALL_ROOT}/share/gromacs/*
+chmod 777 ${INSTALL_ROOT}/share
+chmod 666 ${INSTALL_ROOT}/share/gromacs_bench.sh
+chmod 777 ${INSTALL_ROOT}/share/gromacs
+chmod 666 ${INSTALL_ROOT}/share/gromacs/*
